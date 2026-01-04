079176c25925662c2b215d00a03dd50b99f72132
Azeez Mumeen <azeezmumeen6@gmail.com>
2026-01-04 12:48:45 +0100

    try localhost for facilitator

--- src/routes/base.router.ts
+++ src/routes/base.router.ts
@@ -43,7 +43,7 @@ router.use(
       },
     },
 
-    { url: `${config.appURL}/facilitator` as any }
+    { url: `http://localhost${config.port}/facilitator` as any }
     // { url: "https://pay.x402.jobs" as any }
   )
 );
